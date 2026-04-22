182bcde8cdd9284f291558828f6f179a2337db99
Mibrahimsaeed <mibrahimsaeed2@gmail.com>
2026-04-22 15:54:38 +0500

    Remove testt.py and update gitignore

--- testt.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAVmKQX9dYsA557_a1qQlr66YMlPjbZqP4")
-
-for m in genai.list_models():
-    print(m.name, m.supported_generation_methods)
